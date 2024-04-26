e146467ce2dc11ebeafa10aa18323239837828ae
DionnaW <158370758+DionnaW@users.noreply.github.com>
2024-04-26 17:14:38 -0400

    had to change api plans

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ have my login.jsx functuality working without errors
 i think the project is complete! waiting to speak with the teacher then i will commit and submit
 having firebase errors at deploy checking to see if importing the firebase config will cure this issue
 that didn't help so i'm goin to expose my firebase imfo to se if that works!
+It didn't work so i fetched a pokemon api for the sake of the project AND we actually have a pikachu flavor so.....WIN WIN!
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,39 +3,38 @@ import { useAuth } from '../context/AuthContext';
 
 const Home = () => {
     const { currentUser } = useAuth();
-    const [holidays, setHolidays] = useState([]);
+    const [pokemon, setPokemon] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const url = "https://calendarific.com/api/v2/holidays?api_key=FqsNz5P9tM1gQklpYqBC0aql9izvUikv&country=US&year=2024";
+        const url = "https://pokeapi.co/api/v2/pokemon/pikachu";
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                if (data.response && data.response.holidays) {
-                    setHolidays(data.response.holidays);
-                } else {
-                    console.error('Invalid holidays data:', data);
-                }
+                setPokemon(data);
                 setLoading(false);
             })
             .catch(error => {
-                console.error('Error fetching holidays:', error);
+                console.error('Error fetching Pokemon:', error);
                 setLoading(false);
             });
     }, []);
 
     return (
         <div className='text-2xl font-bold pt-14'>
-            {loading ? <p>Loading holidays...</p> :
+            {loading ? <p>Loading Pokemon...</p> :
                 <>
                     <p>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</p>
-                    <p>SORRY, BUT WE DON'T HAVE ANY OPENINGS THIS YEAR! HERE ARE SOME HOLIDAY EVENTS YOU COULD FIND US AT</p>
-                    <h2>Holidays in the US:</h2>
-                    <ul>
-                        {holidays.map((holiday, index) => (
-                            <li key={index}>{holiday.name} - {holiday.date.iso}</li>
-                        ))}
-                    </ul>
+                    <p>SORRY WE'RE ALL BOOKED FOR THIS YEAR, BUT HERE'S ONE OF OUR FAVORITE <br/> SNOW CONE CHARACTERS</p>
+                    <h2>Pokemon Data:</h2>
+                    {pokemon && (
+                        <div>
+                            <p>Name: {pokemon.name}</p>
+                            <p>Height: {pokemon.height}</p>
+                            <p>Weight: {pokemon.weight}</p>
+                            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                        </div>
+                    )}
                 </>
             }
         </div>
@@ -43,3 +42,46 @@ const Home = () => {
 }
 
 export default Home;
+
+// const Home = () => {
+    // const { currentUser } = useAuth();
+    // const [holidays, setHolidays] = useState([]);
+    // const [loading, setLoading] = useState(true);
+// 
+    // useEffect(() => {
+        // const url = "https://calendarific.com/api/v2/holidays?api_key=FqsNz5P9tM1gQklpYqBC0aql9izvUikv&country=US&year=2024";
+        // fetch(url)
+            // .then(response => response.json())
+            // .then(data => {
+                // if (data.response && data.response.holidays) {
+                    // setHolidays(data.response.holidays);
+                // } else {
+                    // console.error('Invalid holidays data:', data);
+                // }
+                // setLoading(false);
+            // })
+            // .catch(error => {
+                // console.error('Error fetching holidays:', error);
+                // setLoading(false);
+            // });
+    // }, []);
+// 
+    // return (
+        // <div className='text-2xl font-bold pt-14'>
+            {/* {loading ? <p>Loading holidays...</p> : */}
+                // <>
+                    {/* <p>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</p> */}
+                    {/* <p>SORRY, BUT WE DON'T HAVE ANY OPENINGS THIS YEAR! HERE ARE SOME HOLIDAY EVENTS YOU COULD FIND US AT</p> */}
+                    {/* <h2>Holidays in the US:</h2> */}
+                    {/* <ul> */}
+                        {/* {holidays.map((holiday, index) => ( */}
+                            // <li key={index}>{holiday.name} - {holiday.date.iso}</li>
+                        // ))}
+                    {/* </ul> */}
+                {/* </> */}
+            // }
+        {/* </div> */}
+    // );
+// }
+// 
+// export default Home;
